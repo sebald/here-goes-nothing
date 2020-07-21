8d415dadf465185566fad913d36aeda115f583d3
Sebastian Sebald <sebastian.sebald@reservix.de>
2020-07-21 11:56:34 +0200

    update consent banner

---
+++ src/gatsby-theme-blog/components/consent-banner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useThemeUI } from 'theme-ui';
+import CookieConsent from 'react-cookie-consent';
+
+const ConsentBanner = () => {
+  const { theme } = useThemeUI();
+  const { space, colors } = theme;
+
+  return (
+    <CookieConsent
+      location="bottom"
+      buttonText="Accept 🍪"
+      declineButtonText="Decline"
+      cookieName="constent-cookie"
+      style={{
+        background: colors.highlight,
+        color: colors.text,
+      }}
+      contentStyle={{
+        margin: `${space[3]}px 0 ${space[3]}px ${space[4]}px`,
+      }}
+      buttonStyle={{
+        color: colors.white,
+        fontSize: 'inherit',
+        background: colors.primary,
+        padding: `${space[2]}px ${space[4]}px`,
+      }}
+    >
+      This site doesn't use any cookies to track you but embeds third party
+      content which does (Twitch).
+    </CookieConsent>
+  );
+};
+
+export default ConsentBanner;
--- src/gatsby-theme-blog/components/layout.js
+++ src/gatsby-theme-blog/components/layout.js
@@ -1,37 +1,13 @@
 import React from 'react';
-import { useThemeUI } from 'theme-ui';
-
-import CookieConsent from 'react-cookie-consent';
 import DefaultLayout from 'gatsby-theme-blog/src/components/layout';
 
-const Layout = props => {
-  const { theme } = useThemeUI();
-  const { space, colors } = theme;
+import ConsentBanner from './consent-banner';
 
-  return (
-    <>
-      <DefaultLayout {...props} />
-      <CookieConsent
-        location="bottom"
-        buttonText="Yup!"
-        declineButtonText="Decline"
-        cookieName="constent-cookie"
-        style={{
-          background: colors.highlight,
-          color: colors.text,
-        }}
-        contentStyle={{
-          margin: `${space[3]}px 0 ${space[3]}px ${space[4]}px`,
-        }}
-        buttonStyle={{
-          color: colors.white,
-          background: colors.primary,
-          padding: `${space[2]}px ${space[4]}px`,
-        }}
-      >
-        This site doesn't use any cookies to track you.
-      </CookieConsent>
-    </>
-  );
-};
+const Layout = props => (
+  <>
+    <DefaultLayout {...props} />
+    <ConsentBanner />
+  </>
+);
+
 export default Layout;
